b4dd952d39d0a47841c62738bdf9ebd6bfd15fc9
leeewl <leeewl@163.com>
2025-04-02 20:30:00 +0800

    创建spring boot项目，java17，spring boot3.3.10，依赖选择Spring Web,MySQL Driver,Spring Data JPA，这是项目初始的样子。

---
+++ src/main/java/com/gzy/springboot_user_manager/SpringbootUserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gzy.springboot_user_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUserManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUserManagerApplication.class, args);
+    }
+
+}
