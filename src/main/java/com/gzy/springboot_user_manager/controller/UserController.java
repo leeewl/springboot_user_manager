ab18e3ad07ca537e177f4842a00cdaa239d29c42
leeewl <leeewl@163.com>
2025-04-02 23:26:12 +0800

    response不再简单返回一个字符串，而是状态码，message和data。

--- src/main/java/com/gzy/springboot_user_manager/controller/UserController.java
+++ src/main/java/com/gzy/springboot_user_manager/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.gzy.springboot_user_manager.controller;
 
+import com.gzy.springboot_user_manager.pojo.ResponseMessage;
+import com.gzy.springboot_user_manager.pojo.User;
 import com.gzy.springboot_user_manager.pojo.dto.UserDto;
 import com.gzy.springboot_user_manager.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +18,9 @@ public class UserController {
     // REST
     // 增加
     @PostMapping // URL: localhost:8083/user method: POST
-    public String add(@RequestBody UserDto user){ // 接收参数一般用DTO对象, @RequestBody注解将json数据绑定到对象中
-        userService.add(user);
-        return "SUCCESS!";
+    public ResponseMessage<User> add(@RequestBody UserDto user){ // 接收参数一般用DTO对象, @RequestBody注解将json数据绑定到对象中
+        User userNew = userService.add(user);
+        return ResponseMessage.success(userNew);
     }
 
     // 查询 @GetMapping
