cde08e5b48900dd4c1639ee6881114a6e98113c7
leeewl <leeewl@163.com>
2025-04-03 15:52:45 +0800

    增加参数校验功能

--- src/main/java/com/gzy/springboot_user_manager/controller/UserController.java
+++ src/main/java/com/gzy/springboot_user_manager/controller/UserController.java
@@ -5,6 +5,7 @@ import com.gzy.springboot_user_manager.pojo.User;
 import com.gzy.springboot_user_manager.pojo.dto.UserDto;
 import com.gzy.springboot_user_manager.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,12 @@ public class UserController {
     // REST
     // 增加
     @PostMapping // URL: localhost:8083/user method: POST
-    public ResponseMessage<User> add(@RequestBody UserDto user){ // 接收参数一般用DTO对象, @RequestBody注解将json数据绑定到对象中
+    /*
+     接收参数一般用DTO对象
+     @RequestBody注解将json数据绑定到对象中
+     @Validated注解校验参数
+     */
+    public ResponseMessage<User> add(@Validated @RequestBody UserDto user){
         User userNew = userService.add(user);
         return ResponseMessage.success(userNew);
     }
--- src/main/java/com/gzy/springboot_user_manager/pojo/dto/UserDto.java
+++ src/main/java/com/gzy/springboot_user_manager/pojo/dto/UserDto.java
@@ -1,8 +1,17 @@
 package com.gzy.springboot_user_manager.pojo.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
 public class UserDto {
+
+    @NotBlank(message="用户名不能为空") //去除空格
     private String userName;
+    @NotBlank(message="密码不能为空") //去除空格
+    @Length(min=6,max=12,message="密码长度必须在6-12位之间")
     private String password;
+    @Email(message="邮箱格式不正确")
     private String email;
 
     public String getUserName() {
